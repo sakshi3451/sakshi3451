e14180c7cf3155b6842de645bb653f9c63b3f2e2
sakshi <sakshijain6041@gmail.com>
2022-10-10 21:02:18 +0530

    operator

---
+++ operator.js
@@ -0,0 +1,31 @@
+let a=10;
+let b=20;
+console.log("a + b=",a+b)
+console.log("a - b=",a-b)
+console.log("a * b=",a*b)
+console.log("a / b=",a/b)
+console.log("a ** b=",a**b)
+console.log("a % b =",a%b)
+//assignment operator
+let assignment = 1;
+assignment += 4 //same as assignment =assignment + 4
+console.log(assignment)
+assignment -= 4 // same as assignment =assignment -4
+console.log(asssignment)
+assignment *= 4 // same as assignment = assignment *4
+console.log(assignment)
+//comparison operator
+let comp1=3;
+let comp2=5;
+console.log("comp1 == comp2 is",comp1==comp2)
+console.log("comp1 == comp2 is ",comp1!=comp2) 
+console.log("comp1 == comp2 is ",comp1===comp2)
+console.log("comp1 !== comp2 is ",comp1!==comp2)
+console.log("comp1 > comp2 is ",comp1 > comp2)
+//logical operator
+let x =8;
+let y =9;
+console.log(x<y && x==8)
+console.log(x>y || x==8)
+console.log(!false)
+console.log(!false)
