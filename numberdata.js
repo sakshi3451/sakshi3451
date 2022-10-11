0ad803dc0826b54d4af3a2e8aefcd9e4c9d0ef27
sakshi <sakshijain6041@gmail.com>
2022-10-11 16:57:11 +0530

    numberdatatype

---
+++ numberdata.js
@@ -0,0 +1,6 @@
+const number1 = 6/0;
+console.log(number1); //infinity
+const number2 = -6/0;
+console.log(number2) //-infinity
+const number3 ="abc"/6 // string can not be devided by numbers
+console.log(number3); // nan
