94819f078c20ac96284ac5a9016cf4c3f53340ff
sakshi <sakshijain6041@gmail.com>
2022-10-11 19:28:41 +0530

    datatypes

---
+++ datatype2.js
@@ -0,0 +1,14 @@
+// string
+let name = "sakshi";
+console.log("my string is" + name);
+console.log("data type is" +(type name));
+// number
+let marks = 42;
+console.log("my number is "+ marks);
+console.log("data type is" +(type marks));
+// boolean
+let isDriver = true;
+console.log("data type is" + (type of isDriver));
+
+//null
+let nulvar = null;
