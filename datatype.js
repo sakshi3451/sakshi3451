09d8608ea7187b5c78838327c4045095e107209b
sakshi <sakshijain6041@gmail.com>
2022-10-11 12:26:34 +0530

    datatype

---
+++ datatype.js
@@ -0,0 +1,16 @@
+const a ='a';
+const str ='sakshi';
+const age = 43;
+const price = 32.43
+const ispresent = true;
+let abc;
+let b =null;
+  
+console.log('type of a :',typeof a);
+
+console.log('type of str :',typeof str);
+console.log('type of age :',typeof age);
+console.log('type of price :',typeof price);
+console.log('type of is present :',typeof ispresent);
+console.log('type of abc :',typeof abc);
+console.log('type of b :',typeof b);
