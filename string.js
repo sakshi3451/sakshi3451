fdb2aedc815fdf42f27b044e0ca74900fdc323c4
sakshi <sakshijain6041@gmail.com>
2022-10-13 15:27:30 +0530

    stringdata

---
+++ string.js
@@ -0,0 +1,20 @@
+let str = ' i am from hyderabad
+ let replace = str.replace('hyderabad' , 'banglore'); // replaced
+ console.log(replace);
+
+// trim function
+let strWithWhitespaces='this contains           whitspces ';
+console.log(strWithWhitespaces);
+console.log(strWithWhitespaces.trim());
+
+ 
+
+
+ // uppercase// lower case
+ let string = 'this is a string';
+console.log(string.toUpperCase());
+console.log(string. toLowerCase());
+
+// concat method
+let newString=string.concat('New string');
+console.log(newString); 
