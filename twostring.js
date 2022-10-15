1a68aa1c14a401bfa2e029043e7ed7875050d780
sakshi <sakshijain6041@gmail.com>
2022-10-15 16:14:08 +0530

    twostring

---
+++ twostring.js
@@ -0,0 +1,4 @@
+const str1 = new String(" this is a string one"); // this is a first strng
+const str2 = new String( " this is a string two "); // this is a second string 
+const str3 = str1.concat (str2);
+console.log( "concate string :" + str3);
