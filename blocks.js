be8d194e002eb5614f135fa41f1c60c1f0c32cc6
sakshi <sakshijain6041@gmail.com>
2022-10-20 09:05:06 +0530

    blocks

---
+++ blocks.js
@@ -0,0 +1,42 @@
+// if else 
+    let age = 20;
+    if(age>18){
+        console.log("you can drink cold drink");
+    }else{
+        console.log("you can drink water");
+    
+    }
+// if else if else 
+  let number =30;
+  if(number>20){
+    console.log("pass");
+  }
+  else if (number==4){
+    console.log("number is 4")
+  }
+  else if (number ==7){
+    console.log("num is 7")
+  }
+  else if (number==30){
+    console.log("number is 30")
+  }
+  else {
+    console.log("fail");
+  }
+  // switch case program
+  const tea = 49;
+  switch (tea){
+    case 44:
+      console.log("the values of tea  is 44")  
+        break;
+  case 45:
+    console.log("the values of tea is 45")  
+    break;
+    
+    case 46:
+        console.log("the values of tea is 46")
+        break;
+        default:
+            console.log("the value of tea is none of 44,45,46")
+            break;
+  }
