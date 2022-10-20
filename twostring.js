3d1228c46df57e9407582ebedc0cc62c5c484df5
sakshi <sakshijain6041@gmail.com>
2022-10-20 09:03:42 +0530

    tostring

--- twostring.js
+++ twostring.js
@@ -1,4 +1,2 @@
-const str1 = new String(" this is a string one"); // this is a first strng
-const str2 = new String( " this is a string two "); // this is a second string 
-const str3 = str1.concat (str2);
-console.log( "concate string :" + str3);
+const a = [ "sakshi" , " subhi " , " swati" , "riya"];
+console.log(a.toString());
