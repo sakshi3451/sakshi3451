19d3928725a9617c3aac356208ea9088cad7bf05
sakshi <sakshijain6041@gmail.com>
2022-10-20 09:12:55 +0530

    whiledowhile

---
+++ whiledowhile.js
@@ -0,0 +1,16 @@
+let i =1 , n = 5; // display the number from 1 to 5  // initialize the variable
+while (i<=n){   // while loop from 1 to 5
+    console.log(i);
+    i +=1;
+}
+
+// do while loop  // display the number 
+let j = 0; 
+const number= 5;
+do 
+{
+    console.log(j);
+    j++;
+}
+    while (j<=number);
+
