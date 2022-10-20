c9d59878bc4c1e8a37f385161fcec9bde40d95fe
sakshi <sakshijain6041@gmail.com>
2022-10-20 09:11:07 +0530

    loops

---
+++ loops.js
@@ -0,0 +1,50 @@
+ // for loop
+ for (let  i=0; i<5; i++){
+    console.log(i);
+ }
+
+ // program to display the sum n  of natural number
+ let sum = 0;
+ const n = 100 
+ // looping from i = 1 to n 
+ // each iteration , i is increased by 1 
+ for ( let i =1; i<=n; i++){
+    sum+=i; // sum sum+i
+ }
+ console.log('sum:' , sum);
+
+// for-in  loop
+
+let object = {
+    sakshi : 40,
+    subhi :50,
+    swati : 65,
+    aayu : 76,
+}
+for (let a in object){
+    console.log(a);}
+
+    // for- of loop
+     for (let b of " raj"){
+        console.log(b); 
+     }
+ 
+
+
+   
+   
+
+   
+    
+
+
+
+
+   
+   
+
+
+
+
+
+    
