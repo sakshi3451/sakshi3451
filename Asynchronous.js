915de34f89b71450abc540fadbcb65b9dd0b6578
sakshi <sakshijain6041@gmail.com>
2022-10-28 08:16:14 +0530

    asynchronous

--- Asynchronous.js
+++ Asynchronous.js
@@ -3,28 +3,28 @@ console.log('two');
 console.log('three');
 console.log('forth');
 console.log('fifth');
-setTimeout(() =>console.log('forth'),200);
-console.log('sixth');
+setTimeout(() =>console.log('sixt'),200);
+console.log('seventh');
 
 //2
 console.log('hello');
 console.log('hii');
 console.log('bye');
-setTimeout(() =>console.log('hii'),400);
+setTimeout(() =>console.log('good'),400);
 
 //3 
 console.log( 'hello world');
 setTimeout(() =>console.log('hello world'),7000);
 console.log( 'raja');
-console.log(' rajaram');
-setTimeout(() =>console.log('raja'),300);
+console.log(' ramu');
+setTimeout(() =>console.log('rajaram'),300);
 console.log('aayu');
 
 //4 
 console.log('honesty is the best policy');
 console.log('apple');
 console.log('banana');
-setTimeout(()=>console.log('apple'),100);
+setTimeout(()=>console.log('papaya'),100);
 
 //5
 console.log(1);
