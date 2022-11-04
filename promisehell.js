2b85c98f8391560178791daa6bb09c873e8e527b
sakshi <sakshijain6041@gmail.com>
2022-11-04 07:11:06 +0530

    promise hell

--- promisehell.js
+++ promisehell.js
@@ -25,6 +25,59 @@ promiseOne([11,2,3]).then((result)=>{
 }).catch((errResult)=> {
     console.log(`catch block is :${errResult}`);
 });
+//2
+const promise = new Promise((resolve,reject)=>{
+    console.log('promise called');
+    let obj={
+        name1:'sakshi',
+        userId:12
+    }
+    resolve(obj);
+    reject('something wrong!')
+})
+promise
+.then((value)=>{
+    console.log(value)
+})
+.catch(err=> {
+    console.log("error is :"+err)
+})
+// 3 
+const promise2= new Promise((resolve,reject)=>{
+    console.log("promise");
+    let obj ={
+        name:'ram',
+        dob:20,
+        address:'laxmiward',
+    }
+    resolve(obj);
+    reject(wrong)
+})
+promise2
+.then((value)=>{
+console.log(value)
+})
+.catch(err=>{
+    console.log("err is :"+err)
+})
+
+//5
+const promise5 = new Promise((resolve,reject)=>{
+    console.log("promise5");
+    const obj = {
+        name1:'sta',
+        cast:'Tim',
+    }
+    resolve(obj);
+    reject(wrongname)
+})
+promise
+.then((value)=>{
+    console.log(value)
+})
+.catch(err=>{
+    console.log("err is :"+ err)
+})
 
 
 
