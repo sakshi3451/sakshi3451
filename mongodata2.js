42950efd43b4885ff7591607f679fba48611f764
sakshi <sakshijain6041@gmail.com>
2022-11-16 17:30:40 +0530

    mongodata2

---
+++ mongodata2.js
@@ -0,0 +1,25 @@
+const {MongoClient}=require('mongodb'); // imported the mongoClient
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+  
+const connection = async() => {
+    try{
+        await client.connect(); // wating until connection is established.
+        const collection = client.db('node-3').collection('student3');
+        await collection.insertOne({name:'sakshi',city:'sagar',course:'Nodejs'});
+        return Promise.resolve('Insertion Successful');
+    }
+    catch(err){
+        return Promise.reject(err);
+    }
+}
+connection().then((res)=>{
+    console.log(res);
+}).catch((error) =>{
+    console.log('connection failure');
+}); 
+
+
+
+  
+
