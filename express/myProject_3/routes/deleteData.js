424fc9b60d2cb39363a641cb38d21c7626fe374b
sakshi3451 <115331833+sakshi3451@users.noreply.github.com>
2022-12-14 10:00:03 +0530

    Add files via upload

---
+++ express/myProject_3/routes/deleteData.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const collection = require('../utils/mongoConnection').connection();
+
+/* GET home page. */
+router.get('/deleteData', async function(req, res, next) {
+    const name= req.query.name;
+    try{
+    const deleteResult = await (await collection).deleteOne({name});
+    const data = await (await collection).find().toArray();
+    res.render('showData', { data });
+    
+    } catch(error){
+        console.error(err);
+        res.status(500).send('Some Error in Delete operation');
+    }
+  
+});
+
+module.exports = router;
