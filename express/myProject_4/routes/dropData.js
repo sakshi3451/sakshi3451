aa18bce7325423495da7c4fec9fd311b74c5bc12
sakshi <sakshijain6041@gmail.com>
2023-02-02 16:47:39 +0530

    myProject_4

--- express/myProject_4/routes/dropData.js
+++ express/myProject_4/routes/dropData.js
@@ -6,6 +6,15 @@ var cookieParser = require('cookie-parser');
 router.use(cookieParser());
 const jwt = require('jsonwebtoken');
 
+var Json2csvParser = require('json2csv').Parser;
+const fs = require('fs');
+//const { routes } = require('../app');
+const { error } = require('console');
+var nodemailer=require('nodemailer');
+
+
+
+
 
 router.get('/dropData', function(req,res,next){
     res.render('dropData',{error:null});
@@ -25,7 +34,7 @@ router.post('/dropData',async function (req,res,next)
         console.log(result.recordset[0].Address , result.recordset[0].City);
         console.log(result);
         if(result.recordset !== null && result.recordset !== undefined)
-        {
+     /*   {
           // console.log(result.recordset[0].Address , result.recordset[0].City)
           res.render('dropData',{data:result.recordset});
        }
@@ -44,3 +53,74 @@ router.post('/dropData',async function (req,res,next)
 })
 
 module.exports = router ;
+*/
+
+
+
+{
+var mssql_data = JSON.parse(JSON.stringify(result.recordset));
+        var file_header = ['ClientID', 'Lastname', 'Firstname', 'Address', 'City'];
+        var json_data = new Json2csvParser({file_header});
+        var csv_data = json_data.parse(mssql_data);
+        fs.writeFileSync('./sample_data.csv',csv_data);
+        res.setHeader("Content-Type", "text/csv");
+        res.setHeader("Content-Disposition", "attachment; filename=sample_data.csv");
+        res.status(200).end(csv_data);
+        //res.render('dropData', {data:result.recordset});
+
+       /* }else{
+          res.send("Please check");
+        }
+    });
+    }catch(err) {
+        res.status(500).send('Error in Search Result');
+    }
+
+});
+
+module.exports = router;*/
+
+
+var sender=nodemailer.createTransport({
+  service:'gmail',
+  host:'smtp.gmail.com',
+  port:465,
+  secure:true,
+  auth:{
+    user:"sakshiJain6041@gmail.com",
+    pass:'qqyyzjbwjhqlfkbe',
+  },
+});
+
+
+var mail={
+  from:"sakshijain6041@gmail.com",
+  to:'sakshijain6041@gmail.com',
+  subject:"sending Email using Node.js",
+  text:" hii !",
+  attachments:[
+    {
+      filename:'sample_data.csv',
+      path:"D:/sakshi_practice/sakshi3451/express/myProject_4/sample_data.csv",
+    }]
+    }
+    sender.sendMail(mail,function(error,info){
+      if(error){
+        console.log(error);
+      }else{
+        console.log("Email send successfully: " +info.response);
+      }
+    })
+}
+else{
+  res.send("please select correct data");
+}
+    });
+}catch(err) {
+  console.log(error);
+
+}
+});
+
+module.exports = router;
+
